f1db24dd322435361a3402b6c0b8cdbabaee92a0
Santiago <larriquesantiago@gmail.com>
2024-02-24 20:13:12 -0300

    First commit

---
+++ miller-countdown-diciembre2020/assets/scripts.js
@@ -0,0 +1,26 @@
+var countDownDate = new Date("Oct 15, 2023 19:59:59").getTime();
+
+var x = setInterval(function () {
+  var now = new Date().getTime();
+
+  var distance = countDownDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("demo1").innerHTML = days;
+
+  document.getElementById("demo2").innerHTML = hours;
+
+  document.getElementById("demo3").innerHTML = minutes;
+
+  document.getElementById("demo4").innerHTML = seconds;
+
+  // Countdown terminado
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "Expirado";
+  }
+}, 1000);
